3da82b2ab581e2d529d5eaaaf7ef984d3b69378a
mahmoudazmy1 <azmy4189@gmail.com>
2023-09-12 16:22:34 +0300

    initial project. added empty home_screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home_Screen extends StatelessWidget {
+  static const String routeName = 'homescreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:islami/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Home_Screen.routeName,
+      routes: {
+        Home_Screen.routeName: (context) => Home_Screen(),
+      },
+    );
+  }
+}
